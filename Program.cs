eb03ddd2f8b97d420df46d35e8869fa3dbcb3570
SBM2009 <atibasiddiq@gmail.com>
2022-07-25 16:18:05 +0100

    creating first project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my first GIT Project");
+            Console.WriteLine("Hello freaking word");
+        }
+    }
+}
